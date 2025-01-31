3b42013f4e5a2f8c3f0b5810a66d4f7790a796ea
soh22h <soh22h@gmail.com>
2025-01-31 16:54:23 +0900

    요청 cors 허용 처리

--- application-javascript/app.js
+++ application-javascript/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const { initFabric } = require('./config/fabric');
 const willRoute = require('./routes/willRoute');
 
 const app = express();
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 app.use('/wills', willRoute);
 
 initFabric()
